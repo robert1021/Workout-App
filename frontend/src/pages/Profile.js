1dad328d865ff34793b0e61ffaea1c3590f123dd
robert1021 <robert.sparling196@gmail.com>
2022-06-21 21:23:59 -0400

    Update Profile.js
    Created basic template for profile page

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -1,3 +1,47 @@
+import { Container, Grid, Paper, Button } from "@mui/material"
+
 export default function Page() {
-    return <h1 style={{ marginTop: "15vh" }}>Profile</h1>
+    return (
+        <Container>
+            <Grid container style={{ marginTop: "15vh" }} spacing={1}>
+                <Grid item xs={12} style={{ textAlign: "center" }}>
+                    <Paper sx={{ backgroundColor: 'whitesmoke' }} elevation={3}><h2>Profile Page</h2></Paper>
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                    <Paper sx={{ backgroundColor: 'whitesmoke' }} elevation={3}>
+                        <h2>Username</h2>
+                        <h2>Image</h2>
+                        <h2>User type - User or Admin</h2>
+                        <h2>User rating</h2>
+                        <h2>Member since: Jun 2022</h2>
+                    </Paper>
+                </Grid>
+                <Grid item xs={12} sm={8}>
+                    <Paper sx={{ backgroundColor: 'whitesmoke' }} elevation={3}>
+                        <div>
+                            <h2>User Info</h2>
+                            <Button variant="contained">
+                                Edit profile
+                            </Button>
+                        </div>
+
+                        <Grid container spacing={1}>
+                            <Grid item xs={6} >
+                                <h2>Age</h2>
+                            </Grid>
+                            <Grid item xs={6} >
+                                <h2>Weight</h2>
+                            </Grid>
+                            <Grid item xs={6} >
+                                <h2>Height</h2>
+                            </Grid>
+                            <Grid item xs={6} >
+                                <h2>Location</h2>
+                            </Grid>
+                        </Grid>
+                    </Paper>
+                </Grid>
+            </Grid>
+        </Container>
+    )
 }
