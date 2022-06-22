ab9e6ed2e420fb7300a251852e6812978c3c9482
robert1021 <robert.sparling196@gmail.com>
2022-06-22 09:32:19 -0400

    Update Profile.js

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -1,44 +1,64 @@
-import { Container, Grid, Paper, Button } from "@mui/material"
+import { Container, Grid, Paper, Button, Typography, Divider } from "@mui/material"
 
 export default function Page() {
     return (
         <Container>
-            <Grid container style={{ marginTop: "15vh" }} spacing={1}>
-                <Grid item xs={12} style={{ textAlign: "center" }}>
-                    <Paper sx={{ backgroundColor: 'whitesmoke' }} elevation={3}><h2>Profile Page</h2></Paper>
-                </Grid>
-                <Grid item xs={12} sm={4}>
+            <Grid container sx={{ marginTop: "15vh" }} spacing={1}>
+                <Grid item xs={12} style={{ textAlign: "center", marginBottom: '1vh' }}>
                     <Paper sx={{ backgroundColor: 'whitesmoke' }} elevation={3}>
-                        <h2>Username</h2>
-                        <h2>Image</h2>
-                        <h2>User type - User or Admin</h2>
-                        <h2>User rating</h2>
-                        <h2>Member since: Jun 2022</h2>
+
+                        <Typography variant="h3">Profile</Typography>
+
                     </Paper>
                 </Grid>
-                <Grid item xs={12} sm={8}>
+                <Grid item xs={12} sm={12} md={4} sx={{marginBottom: '1vh'}}>
                     <Paper sx={{ backgroundColor: 'whitesmoke' }} elevation={3}>
-                        <div>
-                            <h2>User Info</h2>
-                            <Button variant="contained">
-                                Edit profile
-                            </Button>
-                        </div>
 
-                        <Grid container spacing={1}>
-                            <Grid item xs={6} >
-                                <h2>Age</h2>
+                        <Container>
+                            <Grid container spacing={1}>
+
+                                <Grid item xs={12}><Typography variant="h4">Username</Typography></Grid>
+                                <Grid item xs={12}><Divider /></Grid>
+                                <Grid item xs={12}><h2>Image</h2></Grid>
+                                <Grid item xs={12}><h2>User type - User or Admin</h2></Grid>
+                                <Grid item xs={12}><h2>User rating</h2></Grid>
+                                <Grid item xs={12}><h2>Member since: Jun 2022</h2></Grid>
+
                             </Grid>
-                            <Grid item xs={6} >
-                                <h2>Weight</h2>
+                        </Container>
+
+                    </Paper>
+                </Grid>
+                <Grid item xs={12} sm={12} md={8} >
+                    <Paper sx={{ backgroundColor: 'whitesmoke' }} elevation={3}>
+
+                        <Container>
+                            <Grid container spacing={1}>
+
+                                <Grid item xs={6}><Typography variant="h4">User Info</Typography></Grid>
+                                <Grid item xs={6}>
+                                    <Button variant="contained">
+                                        Edit profile
+                                    </Button>
+                                </Grid>
+                                <Grid item xs={12}><Divider /></Grid>
+                                <Grid item xs={12}>
+                                    <h2>Email</h2>
+                                </Grid>
+                                <Grid item xs={12} >
+                                    <h2>Age</h2>
+                                </Grid>
+                                <Grid item xs={12} >
+                                    <h2>Weight</h2>
+                                </Grid>
+                                <Grid item xs={12} >
+                                    <h2>Height</h2>
+                                </Grid>
+                                <Grid item xs={12} >
+                                    <h2>Location</h2>
+                                </Grid>
                             </Grid>
-                            <Grid item xs={6} >
-                                <h2>Height</h2>
-                            </Grid>
-                            <Grid item xs={6} >
-                                <h2>Location</h2>
-                            </Grid>
-                        </Grid>
+                        </Container>
                     </Paper>
                 </Grid>
             </Grid>
