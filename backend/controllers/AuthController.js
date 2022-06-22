a3bd01dbac48dfde7f2f89642b59a5ad1c54dcb2
Mohamed Temacini <mahim.temacini@gmail.com>
2022-06-22 12:12:23 -0400

    Added validation checks for sign up

--- backend/controllers/AuthController.js
+++ backend/controllers/AuthController.js
@@ -4,28 +4,40 @@ const jwt = require('jsonwebtoken')
 
 
 const register = async (req, res) => {
+    const email = req.body.email
+    const username = req.body.user
+    const password = req.body.password
+
+    const isUserPresent = await User.findOne({user: username})
+        if(isUserPresent) {
+            return res.status(400).send("Username already taken")
+        }
+    
+    const isEmailPresent = await User.findOne({email: email})
+        if(isEmailPresent) {
+            return res.status(400).send("Email already taken")
+        }
+
     try {
         const salt = await bcrypt.genSalt()
-        const hashedPassword = await bcrypt.hash(req.body.password, salt)
+        const hashedPassword = await bcrypt.hash(password, salt)
 
         let user = new User({
-            email: req.body.email,
-            user: req.body.user,
+            email: email,
+            user: username,
             password: hashedPassword
         })
         user.save()
         .then(() => {
-            res.json({message: "User Added Succesfully!"})
+            res.status(200).send("Registration Complete!")
         })
         .catch((e) => {
-            res.jons({message: "Error with saving", error: e})
+            res.status(400).send("Error with saving")
         })
     } 
     catch {
-        res.send("Error processing credentials")
+        res.status(400).send("Error processing credentials")
     }
-    
-    
 }
 
 module.exports = {
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,8 +1,5 @@
 const express = require('express')
 const router = express.Router()
-const User = require('../models/User')
-const bcrypt = require('bcryptjs')
-
 const AuthController = require('../controllers/AuthController')
 
 
