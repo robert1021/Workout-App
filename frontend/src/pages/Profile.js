9109b96ed71fad4abc2de33894f882f97d9982b7
robert1021 <robert.sparling196@gmail.com>
2022-06-23 00:14:49 -0400

    Update Profile.js

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -1,4 +1,4 @@
-import { Container, Grid, Paper, Button, Typography, Divider } from "@mui/material"
+import { Container, Grid, Box, Paper, Button, Typography, Divider, TextField } from "@mui/material"
 
 export default function Page() {
     return (
@@ -6,62 +6,127 @@ export default function Page() {
             <Grid container sx={{ marginTop: "15vh" }} spacing={1}>
                 <Grid item xs={12} style={{ textAlign: "center", marginBottom: '1vh' }}>
                     <Paper sx={{ backgroundColor: 'whitesmoke' }} elevation={3}>
-
-                        <Typography variant="h3">Profile</Typography>
-
+                        <Typography variant="h4">Profile</Typography>
                     </Paper>
                 </Grid>
-                <Grid item xs={12} sm={12} md={4} sx={{marginBottom: '1vh'}}>
+                <Grid item xs={12} sm={12} md={12} lg={4} sx={{ marginBottom: '1vh' }}>
                     <Paper sx={{ backgroundColor: 'whitesmoke' }} elevation={3}>
-
                         <Container>
                             <Grid container spacing={1}>
-
                                 <Grid item xs={12}><Typography variant="h4">Username</Typography></Grid>
                                 <Grid item xs={12}><Divider /></Grid>
                                 <Grid item xs={12}><h2>Image</h2></Grid>
                                 <Grid item xs={12}><h2>User type - User or Admin</h2></Grid>
                                 <Grid item xs={12}><h2>User rating</h2></Grid>
                                 <Grid item xs={12}><h2>Member since: Jun 2022</h2></Grid>
-
                             </Grid>
                         </Container>
-
                     </Paper>
                 </Grid>
-                <Grid item xs={12} sm={12} md={8} >
-                    <Paper sx={{ backgroundColor: 'whitesmoke' }} elevation={3}>
+                <Grid item xs={12} sm={12} md={12} lg={8}>
+                    <Paper sx={{ backgroundColor: 'whitesmoke', paddingBottom: '15px', marginBottom: '1vh' }} elevation={3}>
 
                         <Container>
                             <Grid container spacing={1}>
 
                                 <Grid item xs={6}><Typography variant="h4">User Info</Typography></Grid>
                                 <Grid item xs={6}>
-                                    <Button variant="contained">
-                                        Edit profile
-                                    </Button>
+                                    <Box
+                                        display="flex"
+                                        justifyContent="flex-end"
+                                        alignItems="flex-end"
+                                    >
+                                        <Button variant="contained">
+                                            Edit profile
+                                        </Button>
+                                    </Box>
                                 </Grid>
                                 <Grid item xs={12}><Divider /></Grid>
                                 <Grid item xs={12}>
-                                    <h2>Email</h2>
+                                    <Grid container>
+                                        <Grid item xs={4}>
+                                            <Typography variant="h5">
+                                                Email
+                                            </Typography>
+                                        </Grid>
+                                        <Grid item xs={8}>
+                                            <TextField
+                                                fullWidth
+                                                disabled
+                                                label="testing"
+                                            />
+                                        </Grid>
+                                    </Grid>
                                 </Grid>
                                 <Grid item xs={12} >
-                                    <h2>Age</h2>
+                                    <Grid container>
+                                        <Grid item xs={4}>
+                                            <Typography variant="h5">
+                                                Height
+                                            </Typography>
+                                        </Grid>
+                                        <Grid item xs={8}>
+                                            <TextField
+                                                fullWidth
+                                                disabled
+                                                label="testing"
+                                            />
+                                        </Grid>
+                                    </Grid>
                                 </Grid>
                                 <Grid item xs={12} >
-                                    <h2>Weight</h2>
+                                    <Grid container>
+                                        <Grid item xs={4}>
+                                            <Typography variant="h5">
+                                                Weight
+                                            </Typography>
+                                        </Grid>
+                                        <Grid item xs={8}>
+                                            <TextField
+                                                fullWidth
+                                                disabled
+                                                label="testing"
+                                            />
+                                        </Grid>
+                                    </Grid>
                                 </Grid>
                                 <Grid item xs={12} >
-                                    <h2>Height</h2>
+                                    <Grid container>
+                                        <Grid item xs={4}>
+                                            <Typography variant="h5">
+                                                Location
+                                            </Typography>
+                                        </Grid>
+                                        <Grid item xs={8}>
+                                            <TextField
+                                                fullWidth
+                                                disabled
+                                                label="testing"
+                                            />
+                                        </Grid>
+                                    </Grid>
                                 </Grid>
-                                <Grid item xs={12} >
-                                    <h2>Location</h2>
+                                <Grid item xs={12}>
+                                    <Grid container>
+                                        <Grid item xs={4}>
+                                            <Typography variant="h5">
+                                                Years Trained
+                                            </Typography>
+                                        </Grid>
+                                        <Grid item xs={8}>
+                                            <TextField
+                                                fullWidth
+                                                disabled
+                                                label="testing"
+                                            />
+                                        </Grid>
+                                    </Grid>
                                 </Grid>
                             </Grid>
                         </Container>
-                    </Paper>
-                </Grid>
-            </Grid>
-        </Container>
+                    </Paper >
+                </Grid >
+            </Grid >
+        </Container >
     )
 }
