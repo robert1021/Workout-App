840be96c0fcc95b7b0fc1c61528958ceb95b5164
robert1021 <robert.sparling196@gmail.com>
2022-06-25 01:21:13 -0400

    Made log in page responsive

--- frontend/src/components/Forms/LogInForm.js
+++ frontend/src/components/Forms/LogInForm.js
@@ -1,32 +1,57 @@
 import "./Forms.css";
 import "./LogInForm.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Container, Stack, Paper, TextField, Button, InputAdornment } from "@mui/material";
 import PersonIcon from '@mui/icons-material/Person';
 import KeyIcon from '@mui/icons-material/Key';
 import { useState } from "react";
 import axios from "axios";
+import BasicSnackBar from "../Notifications/BasicSnackBar";
+import BasicBackdrop from "../Loading/BasicBackdrop";
 
 export default function LogInForm() {
+
+  const nav = useNavigate()
+
+  const [severity, setSeverity] = useState('')
+  const [showSnackBar, setShowSnackBar] = useState(false)
+
   // manage state for input fields
   const [emailUser, setEmailUser] = useState("");
   const [password, setPassword] = useState("");
   // mamange state for input error
   const [emailUserError, setEmailUserError] = useState(false);
   const [passwordError, setPasswordError] = useState(false);
+  const [emailUserErrorMsg, setEmailUserErrorMsg] = useState('');
+  const [passwordErrorMsg, setPasswordErrorMsg] = useState('');
+
+  const redirectSuccessfulRegister = () => {
+    // show notification
+    setSeverity('success')
+    setShowSnackBar(true)
+    // should redirect to a new page
+    setTimeout(() => {
+      nav('/profile')
+    }, 3000)
+  }
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    setShowSnackBar(false)
     // set both inputs false so red outline goes away
     setEmailUserError(false);
     setPasswordError(false);
+    setEmailUserErrorMsg('')
+    setPasswordErrorMsg('')
 
     if (emailUser === "") {
       setEmailUserError(true);
+      setEmailUserErrorMsg('Please enter a username or password')
     }
 
     if (password === "") {
       setPasswordError(true);
+      setPasswordErrorMsg('Please enter a password')
     }
 
     // do something if email and password
@@ -38,11 +63,24 @@ export default function LogInForm() {
           password: password
         }
       ).then(res => {
-        alert(res.data)
+
+        redirectSuccessfulRegister()
+
       })
-      .catch(error => {
-        alert(error.response.data)
-      })
+        .catch(error => {
+
+          if (error.response.data === 'User not found') {
+            setEmailUserErrorMsg(error.response.data)
+            setEmailUserError(true)
+
+          } else if (error.response.data === 'Wrong combination of username and password') {
+            setEmailUserErrorMsg(error.response.data)
+            setPasswordErrorMsg(error.response.data)
+            setEmailUserError(true)
+            setPasswordError(true)
+          }
+
+        })
     }
   };
 
@@ -70,6 +108,7 @@ export default function LogInForm() {
               variant="outlined"
               required
               error={emailUserError}
+              helperText={emailUserErrorMsg}
             />
           </div>
           <div className="form-group">
@@ -89,6 +128,7 @@ export default function LogInForm() {
               variant="outlined"
               required
               error={passwordError}
+              helperText={passwordErrorMsg}
             />
           </div>
           <h4 className="forgot-password">
@@ -106,6 +146,10 @@ export default function LogInForm() {
             <Link to={"/signup"}>Sign up</Link>
           </h3>
         </form>
+
+        {showSnackBar && <BasicSnackBar severity={severity} message='Login successful!' duration={3000} />}
+        {showSnackBar && <BasicBackdrop timer={3000} />}
+
       </Paper>
     </Container>
   );
