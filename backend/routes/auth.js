bcff024d80eb701c3c0c80d07fcad7c3728631e8
Mohamed Temacini <mahim.temacini@gmail.com>
2022-06-25 23:39:29 -0400

    Added jwt authentication middleware

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const router = express.Router()
 const AuthController = require('../controllers/AuthController')
+const authenticate = require('../middleware/authenticate')
 
 
 router.post('/register', AuthController.register)
 
 router.post('/login', AuthController.login)
 
-router.get('/', (req, res) => {
+router.get('/', authenticate.authenticate, (req, res) => {
     res.send("Testing that this works")
 })
 
