7cbf4dd0c47436eabf9dfc54272a89ec50a0c653
robert1021 <robert.sparling196@gmail.com>
2022-07-06 19:33:11 -0400

    When history button clicked hides workout log and shows history

--- frontend/src/pages/AddExercise.tsx
+++ frontend/src/pages/AddExercise.tsx
@@ -29,6 +29,12 @@ interface WorkoutData {
     log: AllSetData[]
 }
 
+// const exercises = [
+//     { name: 'squat' },
+//     { name: 'deadlift' }
+
+// ]
+
 
 export const AddExercise: React.FC = () => {
 
@@ -45,6 +51,13 @@ export const AddExercise: React.FC = () => {
     // array of objects that holds set data
     const [allSetData, setAllSetData] = useState<AllSetData[]>([])
 
+
+    const [tabValue, setTabValue] = useState<number>(0);
+
+    const handleTabChange = (newValue: number) => {
+        setTabValue(newValue);
+    };
+
     useEffect(() => {
         getExercises()
     }, [])
@@ -55,7 +68,6 @@ export const AddExercise: React.FC = () => {
         let count: number = 1
 
         for (let set of allSetData) {
-            // id ++
             if (set.exercise === searchBarValue) {
                 count++
             }
@@ -113,7 +125,7 @@ export const AddExercise: React.FC = () => {
         }
 
         console.log(workoutData)
-        
+
         try {
             await axios.post("http://localhost:4000/workout/log", workoutData)
             console.log("Workout posted")
@@ -132,184 +144,199 @@ export const AddExercise: React.FC = () => {
 
 
     return (
+        <>
+            <Container sx={{ marginTop: '15vh' }}>
+                <Paper elevation={3} >
 
-        <Container sx={{ marginTop: '15vh' }}>
-            <Paper elevation={3} >
-                <Grid container sx={{ paddingLeft: '10px', paddingRight: '10px' }}>
+                    {/* add exercise view tab */}
+                    <div style={{ display: tabValue === 0 ? 'block' : 'none' }}>
+                        <Grid container sx={{ paddingLeft: '10px', paddingRight: '10px' }}>
 
-                    <Grid item xs={12} sx={{ marginTop: '10px', marginBottom: '5px' }}>
+                            <Grid item xs={12} sx={{ marginTop: '10px', marginBottom: '5px' }}>
 
-                        <Grid container>
-                            <Grid item xs={8}>
-                                <BasicSearchBar dataset={exercises} datasetSearchKey={'name'} size={"normal"} label={'Search exercise'} setValue={setSearchBarValue} />
+                                <Grid container>
+                                    <Grid item xs={8}>
+                                        <BasicSearchBar dataset={exercises} datasetSearchKey={'name'} size={"normal"} label={'Search exercise'} setValue={setSearchBarValue} />
+                                    </Grid>
+
+                                    <Grid item xs={4}>
+                                        <Box display="flex" justifyContent="flex-end" sx={{ marginTop: '5px' }}>
+                                            <CircleExitButton size={'small'} func={navigateToExercise} />
+                                        </Box>
+                                    </Grid>
+
+                                </Grid>
                             </Grid>
 
-                            <Grid item xs={4}>
-                                <Box display="flex" justifyContent="flex-end" sx={{ marginTop: '5px' }}>
-                                    <CircleExitButton size={'small'} func={navigateToExercise} />
-                                </Box>
+                            <Grid item xs={12}>
+                                <Divider />
+                            </Grid>
+
+                            <Grid item xs={12} sx={{ marginTop: '10px' }}>
+
+                                <Grid container>
+
+                                    <Grid item xs={4}>
+
+
+                                        <BasicDatePicker size={"small"} label={"Date"} setDate={setDate} />
+
+
+                                    </Grid>
+
+                                    <Grid item xs={4} alignContent={'center'}>
+
+
+                                        <h5>{currlocal}</h5>
+
+
+                                    </Grid>
+
+                                    <Grid item xs={4}>
+
+                                        <Box display="flex" justifyContent="flex-end">
+                                            <Button variant="outlined" onClick={() => handleTabChange(1)}>History</Button>
+                                        </Box>
+
+                                    </Grid>
+
+                                </Grid>
+
+                            </Grid>
+
+                            <Grid item xs={12} md={4} sx={{ marginTop: '10px' }}>
+
+
+                                <Grid container justifyContent="center" alignItems="center" spacing={0}>
+
+                                    <Grid item xs={4}>
+                                        <Box display="flex" justifyContent="flex-end" sx={{ paddingRight: '15px' }}>
+                                            <Typography variant="h4" sx={{ marginTop: '5px' }}>Weight</Typography>
+                                        </Box>
+
+                                    </Grid>
+
+                                    <Grid item xs={6}>
+                                        <TextField onBlur={(e) => setWeightValue(e.target.value)} />
+
+                                    </Grid>
+
+                                </Grid>
+
+                            </Grid>
+
+                            <Grid item xs={12} md={4} sx={{ marginTop: '10px' }}>
+
+                                <Grid container justifyContent="center" alignItems="center" spacing={0}>
+
+                                    <Grid item xs={4}>
+                                        <Box display="flex" justifyContent="flex-end" sx={{ paddingRight: '15px' }}>
+                                            <Typography variant="h4" sx={{ marginTop: '5px' }}>Reps</Typography>
+                                        </Box>
+
+                                    </Grid>
+
+                                    <Grid item xs={6}>
+                                        <TextField onBlur={(e) => setRepsValue(e.target.value)} />
+
+                                    </Grid>
+
+                                </Grid>
+
+                            </Grid>
+
+                            <Grid item xs={12} md={4} sx={{ marginTop: '10px' }}>
+
+
+                                <Grid container justifyContent="center" alignItems="center" spacing={0}>
+
+                                    <Grid item xs={4}>
+                                        <Box display="flex" justifyContent="flex-end" sx={{ paddingRight: '15px' }}>
+                                            <Typography variant="h4" sx={{ marginTop: '5px' }}>RPE</Typography>
+                                        </Box>
+                                    </Grid>
+
+                                    <Grid item xs={6}>
+                                        <TextField onBlur={(e) => setRpeValue(e.target.value)} />
+
+                                    </Grid>
+
+                                </Grid>
+
+                            </Grid>
+
+                            <Grid item xs={12} sx={{ paddingBottom: '10px', paddingTop: '10px' }}>
+
+                                <Divider />
+
+                            </Grid>
+
+                            <Grid item xs={12} sx={{ marginBottom: '10px' }}>
+
+                                <Grid container>
+
+                                    <Grid item xs={8}>
+
+                                        <Stack
+                                            direction="row"
+                                            alignItems="baseline"
+                                        >
+                                            <AddButton text={'Add Set'} size={'small'} func={onClickAddSetHandler} />
+
+
+                                        </Stack>
+
+                                    </Grid>
+
+                                    <Grid item xs={4}>
+                                        <Box display="flex" justifyContent="flex-end" sx={{ marginRight: '10px' }}>
+
+                                            <CircleCheckButton size={'small'} func={doneAddingSetData} />
+
+
+                                        </Box>
+
+                                    </Grid>
+
+                                </Grid>
+
+                            </Grid>
+
+                            <Grid item xs={12}>
+
+                                {getChosenExercises().map((data, key) => {
+
+                                    // if (data.set > 1) return null
+
+                                    return (
+                                        <ExerciseAccordion title={data} key={key} allExerciseSetData={allSetData} setAllExerciseSetData={setAllSetData} />
+                                    )
+                                })}
+
                             </Grid>
 
                         </Grid>
-                    </Grid>
 
-                    <Grid item xs={12}>
-                        <Divider />
-                    </Grid>
+                        {showSnackBar && <BasicSnackBar severity={'success'} message='Set added!' duration={250} trigger={setShowSnackBar} />}
+                    </div>
 
-                    <Grid item xs={12} sx={{marginTop: '10px'}}>
 
-                        <Grid container>
+                    {/* history tab */}
+                    <div style={{ display: tabValue === 1 ? 'block' : 'none' }}>
 
-                            <Grid item xs={4}>
+                        <Box display="flex" justifyContent="flex-end">
+                            <Button variant="outlined" onClick={() => handleTabChange(0)}>Workout</Button>
+                        </Box>
 
+                        <h1>{searchBarValue}</h1>
 
-                                <BasicDatePicker size={"small"} label={"Date"} setDate={setDate}/>
+                    </div>
 
+                </Paper>
 
-                            </Grid>
+            </Container>
 
-                            <Grid item xs={4} alignContent={'center'}>
-
-
-                                <h5>{currlocal}</h5>
-
-
-                            </Grid>
-
-                            <Grid item xs={4}>
-
-                                <Box display="flex" justifyContent="flex-end">
-                                    <Button variant="outlined">History</Button>
-                                </Box>
-
-                            </Grid>
-
-
-
-                        </Grid>
-
-                    </Grid>
-
-                    <Grid item xs={12} md={4} sx={{ marginTop: '10px' }}>
-
-
-                        <Grid container justifyContent="center" alignItems="center" spacing={0}>
-                            
-                            <Grid item xs={4}>
-                                <Box display="flex" justifyContent="flex-end" sx={{ paddingRight: '15px' }}>
-                                    <Typography variant="h4" sx={{marginTop: '5px'}}>Weight</Typography>
-                                </Box>
-
-                            </Grid>
-
-                            <Grid item xs={6}>
-                                <TextField onBlur={(e) => setWeightValue(e.target.value)} />
-
-                            </Grid>
-
-                        </Grid>
-
-                    </Grid>
-
-                    <Grid item xs={12} md={4} sx={{ marginTop: '10px' }}>
-
-                        <Grid container justifyContent="center" alignItems="center" spacing={0}>
-                        
-                            <Grid item xs={4}>
-                                <Box display="flex" justifyContent="flex-end" sx={{ paddingRight: '15px' }}>
-                                    <Typography variant="h4" sx={{marginTop: '5px'}}>Reps</Typography>
-                                </Box>
-
-                            </Grid>
-
-                            <Grid item xs={6}>
-                                <TextField onBlur={(e) => setRepsValue(e.target.value)} />
-
-                            </Grid>
-
-                        </Grid>
-
-                    </Grid>
-
-                    <Grid item xs={12} md={4} sx={{ marginTop: '10px' }}>
-
-
-                        <Grid container justifyContent="center" alignItems="center" spacing={0}>
-
-                            <Grid item xs={4}>
-                                <Box display="flex" justifyContent="flex-end" sx={{ paddingRight: '15px' }}>
-                                    <Typography variant="h4" sx={{marginTop: '5px'}}>RPE</Typography>
-                                </Box>
-                            </Grid>
-
-                            <Grid item xs={6}>
-                                <TextField onBlur={(e) => setRpeValue(e.target.value)} />
-
-                            </Grid>
-
-                        </Grid>
-
-                    </Grid>
-
-                    <Grid item xs={12} sx={{ paddingBottom: '10px', paddingTop: '10px' }}>
-
-                        <Divider />
-
-                    </Grid>
-
-                    <Grid item xs={12} sx={{ marginBottom: '10px' }}>
-
-                        <Grid container>
-
-                            <Grid item xs={8}>
-
-                                <Stack
-                                    direction="row"
-                                    alignItems="baseline"
-                                >
-                                    <AddButton text={'Add Set'} size={'small'} func={onClickAddSetHandler} />
-
-
-                                </Stack>
-
-                            </Grid>
-
-                            <Grid item xs={4}>
-                                <Box display="flex" justifyContent="flex-end" sx={{ marginRight: '10px' }}>
-
-                                    <CircleCheckButton size={'small'} func={doneAddingSetData} />
-
-
-                                </Box>
-
-                            </Grid>
-
-                        </Grid>
-
-                    </Grid>
-
-                    <Grid item xs={12}>
-
-                        {getChosenExercises().map((data, key) => {
-
-                            // if (data.set > 1) return null
-
-                            return (
-                                <ExerciseAccordion title={data} key={key} allExerciseSetData={allSetData} setAllExerciseSetData={setAllSetData} />
-                            )
-                        })}
-
-                    </Grid>
-
-                </Grid>
-
-                {showSnackBar && <BasicSnackBar severity={'success'} message='Set added!' duration={250} trigger={setShowSnackBar} />}
-
-            </Paper>
-
-        </Container>
-
+        </>
 
     )
 }
